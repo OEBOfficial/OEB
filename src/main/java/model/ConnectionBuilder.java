c37e37b9ecd5a85d289cfe3f962cac751214da00
NBFSS03 <NBFSS03@192.168.200.229>
2017-06-07 17:14:24 +0700

    edited connectionbuilder

--- src/main/java/model/ConnectionBuilder.java
+++ src/main/java/model/ConnectionBuilder.java
@@ -7,9 +7,9 @@ import java.sql.DriverManager;
 public class ConnectionBuilder {
     public static Connection getConnection(){
         Connection con = null;
-        String url = "jdbc:mysql://iambighead.com:3306/OEB?characterEncoding=UTF-8";
+        String url = "jdbc:mysql://52.43.248.108:3306/OEB?characterEncoding=UTF-8";
         String username = "OrderEatBill";
-        String password = "OEBProject2017";
+        String password = "OEBProject";
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url,username,password);
